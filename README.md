0c17ce4d505cf62a59510455daa14262ea2b5541
Daniel Miessler <daniel@danielmiessler.com>
2015-08-14 12:04:22 -0700

    Updated README

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # caparser
 
 A quick and dirty PCAP parser created to assist network traffic analysis in IoT and Mobile security assessments, *caparse* shows you where your applications are sending cleartext sensitive data.
+
+## About
+
+This tool is created by Daniel Miessler, with help from Brad Wolfe.
--- caparser.sh
+++ caparser.sh
@@ -1,4 +1,4 @@
-## Caparser
+## Caparser: Find plaintext sensitive data in ur netwerk traffics.
 ## By Daniel Miessler
 ## Version 1 sucks a lot. Sorry.
 
@@ -6,15 +6,18 @@
 rm hosts.txt
 rm hostnames.txt 
 rm uniquehosts.txt
+rm pcap_strings.txt
+rm totalhits.txt
+#rm malwarecheck.txt
 
 # Get the hosts list
-tshark -r diphone.pcap -q -z conv,ip 2>/dev/null | cut -d " " -f 1 > hosts.txt 
+tshark -r diphone.pcap -q -z conv,ip 2>/dev/null | grep "^[0-9]" | cut -d " " -f1 > hosts.txt 
 
 # Create a seperate pcap for each host
 for host in `cat hosts.txt`
 do
     tcpdump -r diphone.pcap -w $host.pcap host $host > /dev/null 2>&1
-    strings $host.pcap > $host-strings.txt
+    #strings $host.pcap > $host-strings.txt
 done
 
 NOC=`wc -l hosts.txt | awk '{print $1}'`
@@ -22,43 +25,63 @@ NOC=`wc -l hosts.txt | awk '{print $1}'`
 # Get unique hostnames
 sort hosts.txt | uniq > uniquehosts.txt 
 
+#for host in `cat uniquehosts.txt`
+#do
+#    /Users/daniel/Development/vtscan/vtcheck -u $host >> malwarecheck.txt 
+#done
+
 for host in `cat uniquehosts.txt`
 do
     dig -x $host +short >> hostnames.txt 
 done
 NOH=`wc -l hostnames.txt | awk '{print $1}'`
 
-# Show host list
-echo ""
-echo "============================"
-echo "========== OUTPUT =========="
-echo "============================"
-echo ""
-echo "You made $NOC connections to $NOH hosts."
-echo ""
-echo "*****************"
-echo "*** HOST LIST ***"
-echo "*****************"
-echo ""
-cat hostnames.txt 
-echo ""
-echo "*****************"
 
-for string in `cat sensitive.txt`
-do
-    for pcap in `ls *.pcap`
-    do
-        grep $string $pcap > hit-$pcap.txt
-    done
-done
+# Get strings from the SecLists project
+curl -O https://raw.githubusercontent.com/danielmiessler/SecLists/master/Pattern_Matching/pcap_strings.txt
 
-for string in `cat sensitive_strings.txt`
+# Output based on hits
+for string in `cat pcap_strings.txt`
 do
     echo ""
     echo "There are `tshark -r diphone.pcap | grep -i $string | wc -l | awk '{print $1}'` instances of $string"
+    tshark -r diphone.pcap | grep -i $string | wc -l | awk '{print $1}' >> totalhits.txt
     echo ""
     echo "------- SEARCHING FOR $string ----------"
     tshark -r diphone.pcap | grep -i $string
     echo "------- END $string ----------"
     echo ""
 done
+
+# Output
+
+echo "Scanning pcap…"
+sleep 3
+
+echo ""
+echo "============================"
+echo "========== OUTPUT =========="
+echo "============================"
+
+echo ""
+echo "You made $NOC connections to $NOH hosts."
+echo ""
+
+sleep 2
+
+echo ""
+echo "*** HOST LIST ***"
+echo ""
+sleep 1
+echo "Printing hostnames…"
+sleep 1
+cat hostnames.txt 
+echo ""
+
+sleep 2
+echo "Printing hits…"
+sleep 1
+
+echo "Total instances of sensitive string hits:"
+echo ""
+cat totalhits.txt
